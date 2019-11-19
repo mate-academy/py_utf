28505784a688b6d42da7ea02802f1020ff4e003b
logotip <logotip123@yahoo.com>
2019-11-19 18:36:39 +0200

    ver 1.1

--- convert.py
+++ convert.py
@@ -3,4 +3,18 @@
 
 def utf_len(string: bytes) -> int:
     """len function"""
-    return len(str(string, 'utf-8'))
+    # прибираємо лишні знаки
+    string_without_odd = str(string)[2:-1]
+    result = 0
+    index = 0
+    while index <= len(string_without_odd)-1:
+        result += 1
+        # вичисляємо кирилицю
+        if string_without_odd[index:index+3] == "\\xd":
+            index += 8
+        # вичисляємо китайщину
+        elif string_without_odd[index:index+3] == "\\xe":
+            index += 12
+        else:
+            index += 1
+    return result
