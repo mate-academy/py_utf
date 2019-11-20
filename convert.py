58e4dd111090a3337dcf0070a2002e4e1ba4e519
Artem <artger@ukr.net>
2019-11-20 13:23:19 +0200

    All tests passed but bad code

--- convert.py
+++ convert.py
@@ -1,4 +1,25 @@
-def utf_len(s: bytes) -> int:
+'''
+Module
+'''
+
+LENGTH_BYTE_IN_STR = 10
 
 
-    return 0
+def utf_len(strng: bytes) -> int:
+    '''
+
+    number of characters
+    :param strng:
+    :return:
+    '''
+    count_letter = 0
+    for byte_in_line in strng:
+        # length of the string( byte which is represented in binary form)
+        lngth_binr_byte = len(str(bin(byte_in_line)))
+        if lngth_binr_byte != LENGTH_BYTE_IN_STR:
+            count_letter += 1
+        else:
+            bit = str(bin(byte_in_line)[3])
+            if bit == '1':
+                count_letter += 1
+    return count_letter
