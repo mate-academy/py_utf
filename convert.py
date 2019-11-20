e8698712318e709d7c10c3fb1922533b6bbc251b
d1makov <makovskyj.danylo@gmail.com>
2019-11-20 11:32:21 +0200

    pass

--- convert.py
+++ convert.py
@@ -1,4 +1,22 @@
-def utf_len(s: bytes) -> int:
+"""
+docstring
+"""
 
 
-    return 0
+def utf_len(string: bytes) -> int:
+    """
+
+    :param s:
+    :return:
+    """
+    new_st = str(string)[2:-1].split()
+    count = 0
+    for i in range(len(new_st)):
+        if '\\xd' in new_st[i]:
+            # rus
+            count += len(new_st[i]) // 4 // 2 + len(new_st[i]) % 4
+        elif '\\xe' in new_st[i]:
+            count += len(new_st[i]) // 4 // 3 + len(new_st[i]) % 4
+        else:
+            count += len(new_st[i])
+    return count + str(string).count(' ')
--- test_convert.py
+++ test_convert.py
@@ -1,21 +1,44 @@
+"""
+docstring
+"""
 import convert
 
 
 def test_empty_string():
+    """
+
+    :return:
+    """
     assert convert.utf_len(b"") == 0
 
 
 def test_latin_and_punctuation():
+    """
+
+    :return:
+    """
     assert convert.utf_len(b"Hello!") == 6
 
 
 def test_cyrillic():
+    """
+
+    :return:
+    """
     assert convert.utf_len("Привіт!".encode('utf-8')) == 7
 
 
 def test_japanese():
+    """
+
+    :return:
+    """
     assert convert.utf_len("こんにちは".encode('utf-8')) == 5
 
 
 def test_mixed():
+    """
+
+    :return:
+    """
     assert convert.utf_len("Hello! Привіт! こんにちは".encode('utf-8')) == 20
