fbeba95aa649982f9b7ab565a082667c2f03c875
Platoniuks <platon@alterpost.org>
2019-11-21 08:05:09 +0200

    problem is resolved

--- convert.py
+++ convert.py
@@ -1,4 +1,10 @@
-def utf_len(s: bytes) -> int:
+"""Count symbols in string"""
 
 
-    return 0
+def utf_len(string: bytes):
+    """Count symbols in string"""
+    amount_symbols = 0
+    for byte in string:
+        if byte & 0b10000000 == 0b0 or byte & 0b01000000 == 0b1000000:
+            amount_symbols += 1
+    return amount_symbols
