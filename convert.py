eceb90fcec1419e739409c0916cfce226b149035
Andrii <andrewsotnikov88@gmail.com>
2019-11-22 09:37:36 +0200

    Fixes for convert.py

--- convert.py
+++ convert.py
@@ -1,2 +1,9 @@
-def utf_len(s: bytes) -> int:
-    return len(str(s, 'utf-8'))
+def utf_len(phrase: bytes) -> int:
+    counter = 0
+    if phrase:
+        for i in phrase:
+            if 0b10000000 & i == 0b0000000 or 0b01000000 & i == 0b1000000:
+                counter += 1
+    return counter
+
+
