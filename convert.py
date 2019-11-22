22e1b11c9ad19840fc65a6c2366bd8349c7c8e9a
d1makov <makovskyj.danylo@gmail.com>
2019-11-22 07:56:06 +0200

    revised

--- convert.py
+++ convert.py
@@ -6,17 +6,9 @@ docstring
 def utf_len(string: bytes) -> int:
     """
 
+    :param string:
     :param s:
     :return:
     """
-    new_st = str(string)[2:-1].split()
-    count = 0
-    for i in range(len(new_st)):
-        if '\\xd' in new_st[i]:
-            # rus
-            count += len(new_st[i]) // 4 // 2 + len(new_st[i]) % 4
-        elif '\\xe' in new_st[i]:
-            count += len(new_st[i]) // 4 // 3 + len(new_st[i]) % 4
-        else:
-            count += len(new_st[i])
-    return count + str(string).count(' ')
+    b_len = sum(byte & 0b10000000 == 0b000 or byte & 0b01000000 == 0b01000000 for byte in string)
+    return b_len
