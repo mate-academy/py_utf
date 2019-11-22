c36f6ea4afc53093817d8c54e09daef080f0e363
Andrii <andrewsotnikov88@gmail.com>
2019-11-22 09:40:51 +0200

    Fixes for convert.py with passed linter checks

--- convert.py
+++ convert.py
@@ -1,9 +1,11 @@
+"""docstring"""
+
+
 def utf_len(phrase: bytes) -> int:
+    """docstring for utf-len function"""
     counter = 0
     if phrase:
         for i in phrase:
             if 0b10000000 & i == 0b0000000 or 0b01000000 & i == 0b1000000:
                 counter += 1
     return counter
-
-
